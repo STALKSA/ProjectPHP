832b70104c17099067bc9faf6f790c1e8b1ea5dd
ksenia <ksy184694@gmail.com>
2024-02-23 11:37:29 +0300

    updated mini_website, styles, api

--- api.php
+++ api.php
@@ -14,25 +14,27 @@ if ($receivedToken !== $expectedToken) {
     exit;
 }
 
-// $allowedIP = "VALID_IP_ADDRESS";
-// $clientIP = $_SERVER['REMOTE_ADDR'];
-
-
-// if ($clientIP !== $allowedIP) {
-//     echo json_encode(['error' => 'Неверный IP-адрес']);
-//     exit;
-// }
-
 $conn = new mysqli("127.0.0.1", "root", "root", "exam_db");
 
 if ($conn->connect_error) {
     die("Ошибка подключения: " . $conn->connect_error);
 }
 
+// Функция для подготовки ответа с ошибкой
+function sendError($message)
+{
+    echo json_encode(['error' => $message]);
+    exit;
+}
+
 if (isset($_GET['action']) && $_GET['action'] === 'getUnprocessedOrders') {
     $sql = "SELECT * FROM orders WHERE status = 'новый'";
     $result = $conn->query($sql);
     $unprocessedOrders = [];
+
+    if (!$result) {
+        sendError('Ошибка при выполнении запроса: ' . $conn->error);
+    }
 
     while ($order = $result->fetch_assoc()) {
         $unprocessedOrders[] = $order;
@@ -43,20 +45,36 @@ if (isset($_GET['action']) && $_GET['action'] === 'getUnprocessedOrders') {
 
 if (isset($_GET['action']) && $_GET['action'] === 'getOrderById') {
     $orderId = isset($_GET['orderId']) ? $_GET['orderId'] : '';
+
+    if (!is_numeric($orderId)) {
+        sendError('Некорректный идентификатор заказа');
+    }
+
     $sql = "SELECT * FROM orders WHERE order_id = ?";
     $stmt = $conn->prepare($sql);
 
-    if ($stmt) {
-        $stmt->bind_param("i", $orderId); // "i" - тип данных integer
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $order = $result->fetch_assoc();
-
-        echo json_encode($order);
-        $stmt->close();
-    } else {
-        echo json_encode(['error' => 'Ошибка при подготовке запроса']);
+    if (!$stmt) {
+        sendError('Ошибка при подготовке запроса: ' . $conn->error);
     }
+
+    $stmt->bind_param("i", $orderId); // "i" - тип данных integer
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if (!$result) {
+        sendError('Ошибка при выполнении запроса: ' . $stmt->error);
+    }
+
+    $order = $result->fetch_assoc();
+
+    if ($order) {
+        echo json_encode($order);
+    } else {
+        sendError('Заказ с указанным идентификатором не найден');
+    }
+
+    $stmt->close();
 }
 
 $conn->close();
+
